6bc9c5b95f284151b533457a8ec2b548dcac5ad6
fabriguaglia <fabriguaglia72@gmail.com>
2023-08-16 19:07:55 -0300

    v1.3

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./header.css";
 import imgImport from '../../assets/fotos';
 
 function Header() {
+  const [menuActive, setMenuActive] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuActive(!menuActive);
+  };
+
   return (
     <header className="header">
       <div className="logo">
         <a href="/"><img src={imgImport.plantaLogo} alt=""></img></a>
       </div>
       <div className="titulo">Sembraverde</div>
-      <nav className="nav-links">
+      <div className={`burger ${menuActive ? 'active' : ''}`} onClick={toggleMenu}>
+        {menuActive ? (
+          <div className="close-icon">
+            <div className="bar"></div>
+            <div className="bar"></div>
+          </div>
+        ) : (
+          <div className="bar"></div>
+        )}
+      </div>
+      <nav className={`nav-links ${menuActive ? 'active' : ''}`}>
         <a href="/login">Login</a>
         <a href="/about">Acerca de</a>
         <a href="/contacto">Contáctanos</a>
--- src/components/products/products.jsx
+++ src/components/products/products.jsx
@@ -1,12 +1,80 @@
-import React from 'react'
-import "./products.css"
+import React from 'react';
+import "./products.css";
 
+const productos = [
+  {
+    id: 1,
+    nombre: "Semillas-de-petuñas",
+    imagen: "imagen_1.jpg",
+    precio: 9.99,
+  },
+  {
+    id: 2,
+    nombre: "Semillas-de-margarita",
+    imagen: "imagen_2.jpg",
+    precio: 12.99,
+  },
+  {
+    id: 3,
+    nombre: "Semillas-de-alegría-del-hogar",
+    imagen: "imagen_2.jpg",
+    precio: 12.99,
+  },
+  {
+    id: 4,
+    nombre: "Bulbos-de-fresias",
+    imagen: "imagen_2.jpg",
+    precio: 12.99,
+  },
+  {
+    id: 5,
+    nombre: "Semillas-de-coral",
+    imagen: "imagen_2.jpg",
+    precio: 12.99,
+  },
+  {
+    id: 6,
+    nombre: "Semillas-de-lovelia",
+    imagen: "imagen_2.jpg",
+    precio: 12.99,
+  },
+  {
+    id: 7,
+    nombre: "Semillas-de-copete",
+    imagen: "imagen_2.jpg",
+    precio: 12.99,
+  },
+  {
+    id: 8,
+    nombre: "Semillas-de-",
+    imagen: "imagen_2.jpg",
+    precio: 12.99,
+  },
+  {
+    id: 9,
+    nombre: "Producto 2",
+    imagen: "imagen_2.jpg",
+    precio: 12.99,
+  },
+];
 
 function Products() {
   return (
     <div className='principalproducts'>
-        <h1 className='productstitle'>Conecta con la naturaleza</h1>
+      <h1 className='productstitle'>Conecta con la naturaleza</h1>
+      <div className='catalogo'>
+        {productos.map((producto) => (
+          <div key={producto.id} className='producto'>
+            <a href={`/producto/${producto.nombre}`}>
+              <img src={producto.imagen} alt={producto.nombre} />
+            </a>
+            <p>Precio: ${producto.precio.toFixed(2)}</p>
+            <button>Agregar al carrito</button>
+          </div>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
-export default Products
+
+export default Products;
